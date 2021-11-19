01c8a71611f7bf2309fced6209d50987dcc2ee0a
Isaac Grosof <isaacbg227@gmail.com>
2021-11-19 14:53:45 -0500

    Testing script

---
+++ test.sh
@@ -0,0 +1,25 @@
+#/usr/bin/bash
+echo "Clauses"
+for i in {1..20}
+    do
+    for j in {2..9}
+        do
+        python3 prove-amo-general.py $i $j > testing/amo-$i-$j.drat
+        echo -n `grep -c '^[-0-9]' testing/amo-$i-$j.drat`
+        echo -n ' '
+    done
+    echo ''
+done
+
+echo "Verifying, 0 is success"
+for i in {1..20}
+    do
+    python3 generate.py $i > testing/php-standard-$i.cnf
+    for j in {2..9}
+        do
+        drat/drat-trim testing/php-standard-$i.cnf testing/amo-$i-$j.drat -b >/dev/null
+        echo -n '.'
+    done
+    echo ''
+done
+echo $?
