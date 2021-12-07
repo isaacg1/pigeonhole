082a477058f1961d0948372534713d3703d9c7c4
Isaac Grosof <isaacbg227@gmail.com>
2021-12-06 21:45:39 -0500

    Better test

--- test2.sh
+++ test2.sh
@@ -5,12 +5,11 @@ for i in {1..100}
     python3 prove-amo-general.py $i 3 > testing/amo-$i-3.drat
     echo -n $i `grep -c '^[-0-9]' testing/amo-$i-3.drat`
     python3 generate.py $i > testing/php-standard-$i.cnf
-    drat/drat-trim testing/php-standard-$i.cnf testing/amo-$i-3.drat -b >/dev/null
+    /usr/bin/time -f "\t%E real,\t%U user,\t%S sys" drat/drat-trim testing/php-standard-$i.cnf testing/amo-$i-3.drat -b >/dev/null
     result=$?
     if [[ $result -gt 0 ]]
     then
         echo $result
         exit $result
     fi
-    echo " ."
 done
